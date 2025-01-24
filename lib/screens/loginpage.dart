38168b227a8a80458f075e500ae869d081778a41
Sandwish :) <91787084+isandwish@users.noreply.github.com>
2025-01-24 11:42:33 +0700

    Edit Scroll Down

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -25,56 +25,58 @@ class LoginPage extends StatelessWidget {
               children: [
                 Background(),
                 Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      UBEChallengeLogo(width: 260),
-                      RunLogo(width: 200, height: 175),
-                      SizedBox(height: 40),
-                      TextBox(boxname: 'email'),
-                      SizedBox(height: 20),
-                      PasswordBox(),
-                      SizedBox(height: 20),
-                      ActionButton(
-                        text: 'Login',
-                        width: 130,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => TemplatePage(
-                                      content: HomePage(),
-                                    )),
-                          );
-                        },
-                      ),
-                      SizedBox(height: 20),
-                      RichText(
-                        text: TextSpan(
-                          text: 'No account? ',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFFD9D9D9),
-                          ),
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Create one',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                              ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => RegisterPage()),
-                                  );
-                                },
-                            ),
-                          ],
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        UBEChallengeLogo(width: 260),
+                        RunLogo(width: 200, height: 175),
+                        SizedBox(height: 40),
+                        TextBox(boxname: 'email'),
+                        SizedBox(height: 20),
+                        PasswordBox(),
+                        SizedBox(height: 20),
+                        ActionButton(
+                          text: 'Login',
+                          width: 130,
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => TemplatePage(
+                                        content: HomePage(),
+                                      )),
+                            );
+                          },
                         ),
-                      ),
-                    ],
+                        SizedBox(height: 20),
+                        RichText(
+                          text: TextSpan(
+                            text: 'No account? ',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFFD9D9D9),
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Create one',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => RegisterPage()),
+                                    );
+                                  },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
