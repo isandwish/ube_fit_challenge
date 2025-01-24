72e30334a19f51b05544b8784203fe0e0da032e2
Sandwish :) <91787084+isandwish@users.noreply.github.com>
2025-01-24 11:43:08 +0700

    Edit Scroll Down

--- lib/screens/landingpage.dart
+++ lib/screens/landingpage.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: sized_box_for_whitespace
-
 import 'package:flutter/material.dart';
 import 'package:ube_fit_challenge/widgets/app_logo.dart';
 import 'package:ube_fit_challenge/widgets/background.dart';
@@ -15,53 +13,55 @@ class LandingPage extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Container(
+          child: SizedBox(
             width: 1080,
             height: 2400,
             child: Stack(
               children: [
                 Background(),
                 Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      UBEChallengeLogo(width: 260),
-                      RunLogo(width: 200, height: 175),
-                      SizedBox(height: 40),
-                      SizedBox(
-                        width: 300,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Hey there,\nAre you ready?',
-                              style: TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                                  height: 1,
-                                  color: Colors.white),
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              'Whether you’re a fitness pro or just getting started, we’ve got your back!',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.black),
-                            ),
-                          ],
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        UBEChallengeLogo(width: 260),
+                        RunLogo(width: 200, height: 175),
+                        SizedBox(height: 40),
+                        SizedBox(
+                          width: 300,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Hey there,\nAre you ready?',
+                                style: TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                    height: 1,
+                                    color: Colors.white),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Whether you’re a fitness pro or just getting started, we’ve got your back!',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 30),
-                      ActionButton(
-                        text: 'Let\'s Start',
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()),
-                          );
-                        },
-                      ),
-                    ],
+                        SizedBox(height: 30),
+                        ActionButton(
+                          text: 'Let\'s Start',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
